8d587a6ec26156010bb2b4b581dca18699b52fec
Ramzes <68265608+ramzes-hk@users.noreply.github.com>
2024-09-09 17:56:07 -0400

    fixed url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ramzes-hk.github.io/ak_wordle_solver/",
+  base: "/ak_wordle_solver/",
 })
